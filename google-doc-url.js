1de3ee76bf351b541ec1c7a2e9774d285a985bcf
mitsbatis <mitsbatistakis@gmail.com>
2018-11-10 17:30:46 +0200

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1hnARKItukcqjLa-FNocrUSFxMZpMoL868QT7-fLlIoA/edit?usp=sharing/';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1hnARKItukcqjLa-FNocrUSFxMZpMoL868QT7-fLlIoA/edit#gid=0/';
